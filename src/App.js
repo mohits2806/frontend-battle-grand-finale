1a1c6f21fdd951a2d1fc880edd9e9214cf2d35d6
Mohit Shaharwale <mohitshaharwale01@gmail.com>
2024-05-21 01:17:22 +0530

    Created a outline, and created react components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Feed from './components/Feed';
+import Profile from './components/Profile';
+import EditProfile from './components/EditProfile';
+import Connections from './components/Connections';
+import Message from './components/Message';
+import Notification from './components/Notification';
+import Jobs from './components/Jobs';
+import Company from './components/Company';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Router>
+        <Routes>
+                <Route exact path="/" element={<Feed />} />
+                <Route exact path="/profile" element={<Profile />} />
+                <Route exact path="/profile/edit" element={<EditProfile />} />
+                <Route exact path="/connections" element={<Connections />} />
+                <Route exact path="/message" element={<Message />} />
+                <Route exact path="/notification" element={<Notification />} />
+                <Route exact path="/jobs" element={<Jobs />} />
+                <Route exact path="/company" element={<Company />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Feed.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Feed() {
+  return (
+    <>
+        <h1>Feed</h1>
+        <p>This is the feed page</p>
+    </>
+  )
+}
