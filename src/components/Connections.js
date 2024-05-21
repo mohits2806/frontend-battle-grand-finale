67191d1ec64378be9dc441919f9bd8756e73d552
Mohit Shaharwale <mohitshaharwale01@gmail.com>
2024-05-22 01:57:32 +0530

    connections page complete

--- src/components/Connections.js
+++ src/components/Connections.js
@@ -1,10 +1,285 @@
-import React from 'react'
+import React from 'react';
+import '../style.css';
+import { Link } from 'react-router-dom';
 
 export default function Connections() {
 
   document.title = "Linkwitch - John Doe's connections";
 
   return (
-    <div>Connections</div>
+    <>
+      <section id="connections">
+        <div className='container'>
+          <div className='box box2'>
+            <div className='card'>
+              <h4 style={{marginLeft:'1rem'}}>345 connections</h4>
+              <hr/>
+
+
+{/* Connections */}
+              {/* 1st Connection */}
+              <div className='up'>
+                  <img src='/imgs/elon.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>CEO @Tesla Motors</p>
+                    </div>
+              </div>
+              {/* 2nd Connection */}
+              <div className='up'>
+                  <img src='/imgs/warren.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Warren Buffet</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 3rd Connection */}
+              <div className='up'>
+                  <img src='/imgs/charlie.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Charlie Munger</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Co-Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 4th Connection */}
+              <div className='up'>
+                  <img src='/imgs/virat.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Captain @Royal Challengers Banglore</p>
+                    </div>
+              </div>
+              {/* 5th Connection */}
+              <div className='up'>
+                  <img src='/imgs/mahi.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Mahendra Singh Dhoni</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Cricketer</p>
+                    </div>
+              </div>
+{/* Connections */}
+              {/* 1st Connection */}
+              <div className='up'>
+                  <img src='/imgs/elon.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>CEO @Tesla Motors</p>
+                    </div>
+              </div>
+              {/* 2nd Connection */}
+              <div className='up'>
+                  <img src='/imgs/warren.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Warren Buffet</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 3rd Connection */}
+              <div className='up'>
+                  <img src='/imgs/charlie.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Charlie Munger</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Co-Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 4th Connection */}
+              <div className='up'>
+                  <img src='/imgs/virat.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Captain @Royal Challengers Banglore</p>
+                    </div>
+              </div>
+              {/* 5th Connection */}
+              <div className='up'>
+                  <img src='/imgs/mahi.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Mahendra Singh Dhoni</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Cricketer</p>
+                    </div>
+              </div>
+{/* Connections */}
+              {/* 1st Connection */}
+              <div className='up'>
+                  <img src='/imgs/elon.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>CEO @Tesla Motors</p>
+                    </div>
+              </div>
+              {/* 2nd Connection */}
+              <div className='up'>
+                  <img src='/imgs/warren.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Warren Buffet</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 3rd Connection */}
+              <div className='up'>
+                  <img src='/imgs/charlie.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Charlie Munger</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Co-Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 4th Connection */}
+              <div className='up'>
+                  <img src='/imgs/virat.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Captain @Royal Challengers Banglore</p>
+                    </div>
+              </div>
+              {/* 5th Connection */}
+              <div className='up'>
+                  <img src='/imgs/mahi.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Mahendra Singh Dhoni</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Cricketer</p>
+                    </div>
+              </div>
+{/* Connections */}
+              {/* 1st Connection */}
+              <div className='up'>
+                  <img src='/imgs/elon.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>CEO @Tesla Motors</p>
+                    </div>
+              </div>
+              {/* 2nd Connection */}
+              <div className='up'>
+                  <img src='/imgs/warren.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Warren Buffet</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 3rd Connection */}
+              <div className='up'>
+                  <img src='/imgs/charlie.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Charlie Munger</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Co-Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 4th Connection */}
+              <div className='up'>
+                  <img src='/imgs/virat.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Captain @Royal Challengers Banglore</p>
+                    </div>
+              </div>
+              {/* 5th Connection */}
+              <div className='up'>
+                  <img src='/imgs/mahi.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Mahendra Singh Dhoni</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Cricketer</p>
+                    </div>
+              </div>
+{/* Connections */}
+              {/* 1st Connection */}
+              <div className='up'>
+                  <img src='/imgs/elon.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>CEO @Tesla Motors</p>
+                    </div>
+              </div>
+              {/* 2nd Connection */}
+              <div className='up'>
+                  <img src='/imgs/warren.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Warren Buffet</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 3rd Connection */}
+              <div className='up'>
+                  <img src='/imgs/charlie.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Charlie Munger</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Co-Founder @Berkshire Hathway INC.</p>
+                    </div>
+              </div>
+              {/* 4th Connection */}
+              <div className='up'>
+                  <img src='/imgs/virat.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Captain @Royal Challengers Banglore</p>
+                    </div>
+              </div>
+              {/* 5th Connection */}
+              <div className='up'>
+                  <img src='/imgs/mahi.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'2rem'}}>
+                      <span className='highlight post-name'>Mahendra Singh Dhoni</span>
+                      &nbsp;&#9679;&nbsp;
+                      <Link to='/message' className='nav-links'>Message</Link>
+                      <p className='headline'>Cricketer</p>
+                    </div>
+              </div>
+            </div>
+          </div>
+
+          <div className='box box3'>
+            <p>LinkedIn Corporation © 2024</p>
+            <p>All Rights Reserved</p>
+          </div>
+        </div>
+      </section> 
+    </>
   )
 }
