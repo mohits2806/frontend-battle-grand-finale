77fb5d74aef0ffbb1c005e3e4cbea25454fc3b5b
Mohit Shaharwale <mohitshaharwale01@gmail.com>
2024-05-22 01:39:03 +0530

    feed page completed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,7 +3,7 @@ import '../style.css';
 
 export default function Feed() {
 
-  document.title = "Linkwitch - A best social media app in the world";
+  document.title = "Feed | LinkWitch";
 
   return (
     <>
@@ -34,14 +34,195 @@ export default function Feed() {
               </div>
             </div>
             <hr />
+
+
+            {/* Post 1 */}
             <div className='card'>
-              
+              <div className='post'>
+                <div className='up'>
+                  <img src='/imgs/elon.jpg' alt='elon musk' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'10px'}}>
+                      <span className='highlight post-name'>Elon Musk</span>
+                      &nbsp;&#9679;&nbsp;
+                      <span className='degree'>3rd+</span>
+                      <p className='headline'>CEO @Tesla Motors</p>
+                    </div>
+                </div>
+                <div className='post-title'>
+                  <p>Elon Musk has announced a new project that will revolutionize the world</p>
+                </div>
+                    <img src='/imgs/post1.jpg' alt='post' width={'100%'}/>
+                    <div className='down'>
+                      <div className='count'>
+                        <img src='/imgs/like-count.png' alt='likes' height={20} width={20} style={{borderRadius:'50%'}}/>10470
+                        &nbsp;&#9679;&nbsp;
+                        <span style={{position:'relative', right:'0'}}>242 comments</span>
+                      </div>
+                      <hr/>
+                      <div className='interaction'>
+                        <img src='/imgs/like.png' alt='like' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Like</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/comment.png' alt='comment' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Comment</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/share.png' alt='share' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Share</span>
+                      </div>
+                    </div>
+              </div>
+            </div>
+
+
+
+            {/* Post 2 */}
+            <div className='card'>
+              <div className='post'>
+                <div className='up'>
+                  <img src='/imgs/warren.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'10px'}}>
+                      <span className='highlight post-name'>Warren Buffet</span>
+                      &nbsp;&#9679;&nbsp;
+                      <span className='degree'>2nd+</span>
+                      <p className='headline'>Founder @Berkshire Hathway INC.</p>
+                    </div>
+                </div>
+                <div className='post-title'>
+                  <p>Warren Buffet has announced a new project that will revolutionize the world</p>
+                </div>
+                    <img src='/imgs/post2.jpg' alt='post' width={'100%'}/>
+                    <div className='down'>
+                      <div className='count'>
+                        <img src='/imgs/like-count.png' alt='likes' height={20} width={20} style={{borderRadius:'50%'}}/>1039
+                        &nbsp;&#9679;&nbsp;
+                        <span style={{position:'relative', right:'0'}}>22 comments</span>
+                      </div>
+                      <hr/>
+                      <div className='interaction'>
+                        <img src='/imgs/like.png' alt='like' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Like</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/comment.png' alt='comment' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Comment</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/share.png' alt='share' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Share</span>
+                      </div>
+                    </div>
+              </div>
+            </div>
+
+
+
+            {/* Post 3 */}
+            <div className='card'>
+              <div className='post'>
+                <div className='up'>
+                  <img src='/imgs/charlie.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'10px'}}>
+                      <span className='highlight post-name'>Charlie Munger</span>
+                      &nbsp;&#9679;&nbsp;
+                      <span className='degree'>1st</span>
+                      <p className='headline'>Co-Founder @Berkshire Hathway INC.</p>
+                    </div>
+                </div>
+                <div className='post-title'>
+                  <p>Charlie Munger has announced a new project that will revolutionize the world</p>
+                </div>
+                    <img src='/imgs/post3.jpg' alt='post' width={'100%'}/>
+                    <div className='down'>
+                      <div className='count'>
+                        <img src='/imgs/like-count.png' alt='likes' height={20} width={20} style={{borderRadius:'50%'}}/>12449
+                        &nbsp;&#9679;&nbsp;
+                        <span style={{position:'relative', right:'0'}}>2122 comments</span>
+                      </div>
+                      <hr/>
+                      <div className='interaction'>
+                        <img src='/imgs/like.png' alt='like' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Like</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/comment.png' alt='comment' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Comment</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/share.png' alt='share' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Share</span>
+                      </div>
+                    </div>
+              </div>
+            </div>
+
+
+
+            {/* Post 4 */}
+            <div className='card'>
+              <div className='post'>
+                <div className='up'>
+                  <img src='/imgs/virat.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'10px'}}>
+                      <span className='highlight post-name'>Virat Kohli</span>
+                      &nbsp;&#9679;&nbsp;
+                      <span className='degree'>1st</span>
+                      <p className='headline'>Captain @Royal Challengers Banglore</p>
+                    </div>
+                </div>
+                <div className='post-title'>
+                  <p>Virat Kohli has announced a new project that will revolutionize the world</p>
+                </div>
+                    <img src='/imgs/post4.jpg' alt='post' width={'100%'}/>
+                    <div className='down'>
+                      <div className='count'>
+                        <img src='/imgs/like-count.png' alt='likes' height={20} width={20} style={{borderRadius:'50%'}}/>121449
+                        &nbsp;&#9679;&nbsp;
+                        <span style={{position:'relative', right:'0'}}>2142 comments</span>
+                      </div>
+                      <hr/>
+                      <div className='interaction'>
+                        <img src='/imgs/like.png' alt='like' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Like</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/comment.png' alt='comment' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Comment</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/share.png' alt='share' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Share</span>
+                      </div>
+                    </div>
+              </div>
+            </div>
+
+
+
+            {/* Post 5 */}
+            <div className='card'>
+              <div className='post'>
+                <div className='up'>
+                  <img src='/imgs/mahi.jpg' alt='img' height={50} width={50} style={{borderRadius:'50%', float:'left'}}/>
+                    <div className='mid' style={{marginLeft:'10px'}}>
+                      <span className='highlight post-name'>Mahendra Singh Dhoni</span>
+                      &nbsp;&#9679;&nbsp;
+                      <span className='degree'>2nd</span>
+                      <p className='headline'>Cricketer</p>
+                    </div>
+                </div>
+                <div className='post-title'>
+                  <p>Thala for a reason</p>
+                </div>
+                    <img src='/imgs/post5.jpg' alt='post' width={'100%'}/>
+                    <div className='down'>
+                      <div className='count'>
+                        <img src='/imgs/like-count.png' alt='likes' height={20} width={20} style={{borderRadius:'50%'}}/>21749
+                        &nbsp;&#9679;&nbsp;
+                        <span style={{position:'relative', right:'0'}}>232 comments</span>
+                      </div>
+                      <hr/>
+                      <div className='interaction'>
+                        <img src='/imgs/like.png' alt='like' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Like</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/comment.png' alt='comment' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Comment</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <img src='/imgs/share.png' alt='share' height={20} width={20}/>
+                        &nbsp;<span className='nav-links'>Share</span>
+                      </div>
+                    </div>
+              </div>
             </div>
           </div>
 
           <div className='box box3'>
-            hello
-            <footer></footer>
+            <p>LinkedIn Corporation © 2024</p>
+            <p>All Rights Reserved</p>
           </div>
         </div>
       </section>
