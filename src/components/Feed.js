1ce3a5fd29f1b3afa1856a95b79c34977968032d
Mohit Shaharwale <mohitshaharwale01@gmail.com>
2024-05-21 21:42:31 +0530

    adding middle content in feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -10,27 +10,34 @@ export default function Feed() {
       <section id="feed">
         <div className='container'>
           <div className='box box1'>
-            <img id='cover-img' src="/imgs/cover-img.webp" height={50}/>
-            <img id='pfp' src="/imgs/pfp.png" height={50} width={50}/>
+            <img id='cover-img' src="/imgs/cover-img.webp" alt='cover' height={50}/>
+            <img id='pfp' src="/imgs/pfp.png" alt='pfp' height={70} width={70}/>
             <h2>John Doe</h2>
             <p>Full Stack Developer</p>
             <hr />
-            <p>Profile Views: <span className='highlight'>207</span></p>
+            <p>Profile Viewers: <span className='highlight'>207</span></p>
             <p>Post impressions: <span className='highlight'>367</span></p>
             <hr />
             <p className='nav-links'>Saved items</p>
           </div>
 
-
           <div className='box box2'>
             <div className='card'>
-              <img src="/imgs/pfp.png" height={50} />
-              <h2>Post Title</h2>
-              <p>Post Description</p>
-              <button>Read More</button>
+              <div className='up-card'>
+                <img id='pfp' src="/imgs/pfp.png" alt='pfp' height={50} />
+                <input id='start-post' type='text' placeholder='Start a Post' />
+              </div>
+              <div className='down-card'>
+                <a href='/' className='nav-links'>Home</a>
+                <a href='/' className='nav-links'>Media</a>
+                <a href='/' className='nav-links'>Write a Article</a>
+              </div>
+            </div>
+            <hr />
+            <div className='card'>
+              
             </div>
           </div>
-
 
           <div className='box box3'>
             hello
